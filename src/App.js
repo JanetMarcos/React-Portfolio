e8d7873cdec683a48aba135c96289f9053cf02f6
Astrid M <astrid@astrids-mbp-2.home>
2023-03-02 21:10:58 +0000

    react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div class="top-container" >
+        <div class="title-text">
+          <h1>I'm Janet.</h1>
+          <h2>an aspiring programmer.</h2>
+        </div>
+      </div>
+
+      <div class="middle-container">
+        <div class="profile">
+          <h2>Hello.</h2>
+          <p class="intro">I am a passionate about learning and solving problems.</p>
+          <p> I love creating solutions that will have a positive impact on people's lives</p>
+        </div>
+
+        <div class="hobbies">
+          <h2>My Hobbies.</h2>
+          <div class="hobbies">
+            <h3>YouTuber</h3>
+            <p>I have been running my own youtube channel for the last two years now. It a platform where I engage with my fans</p>
+            <button> <a href="https://www.youtube.com/@janetmarcos707">Click here to visit my youtube channel</a></button>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
